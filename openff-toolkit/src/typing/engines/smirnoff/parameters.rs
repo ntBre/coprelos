26f9e8ad168ef8dd725b6db4b39b3e744478d319
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-04 15:31:51 -0500

    allow doing something with a ParameterHandler

--- openff-toolkit/src/typing/engines/smirnoff/parameters.rs
+++ openff-toolkit/src/typing/engines/smirnoff/parameters.rs
@@ -3,5 +3,22 @@
 
 use pyo3::{FromPyObject, Py, PyAny};
 
+use crate::get_props;
+
 #[derive(FromPyObject)]
 pub struct ParameterHandler(pub(crate) Py<PyAny>);
+
+impl ParameterHandler {
+    get_props! {
+        parameters, Vec<Parameter>;
+    }
+}
+
+#[derive(FromPyObject)]
+pub struct Parameter(pub(crate) Py<PyAny>);
+
+impl Parameter {
+    get_props! {
+        id, String;
+    }
+}
