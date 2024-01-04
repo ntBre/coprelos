3313d942960d8c7dc8daffb03bade28241e83474
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-04 15:08:45 -0500

    get a basic Topology working, with part of Molecule

--- openff-toolkit/src/topology/molecule.rs
+++ openff-toolkit/src/topology/molecule.rs
@@ -1,9 +1,19 @@
-use pyo3::{types::PyModule, FromPyObject, Py, PyAny, Python};
+use openff_units::Quantity;
+use pyo3::{
+    types::{IntoPyDict, PyModule},
+    FromPyObject, IntoPy, Py, PyAny, Python,
+};
 
 const PYMODULE: &str = "openff.toolkit.topology.molecule";
 
 #[derive(FromPyObject)]
 pub struct Molecule(pub(crate) Py<PyAny>);
+
+impl IntoPy<Py<PyAny>> for Molecule {
+    fn into_py(self, _py: Python<'_>) -> Py<PyAny> {
+        self.0
+    }
+}
 
 impl Molecule {
     pub fn new() -> Self {
@@ -30,6 +40,36 @@ impl Molecule {
                 .unwrap()
                 .extract(py)
                 .unwrap()
+        })
+    }
+
+    pub fn add_bond(
+        &mut self,
+        atom1: usize,
+        atom2: usize,
+        bond_order: usize,
+        is_aromatic: bool,
+        fractional_bond_order: f64,
+    ) -> usize {
+        Python::with_gil(|py| {
+            let kwargs = [("fractional_bond_order", fractional_bond_order)]
+                .into_py_dict(py);
+            self.0
+                .call_method(
+                    py,
+                    "add_bond",
+                    (atom1, atom2, bond_order, is_aromatic),
+                    Some(kwargs),
+                )
+                .unwrap()
+                .extract(py)
+                .unwrap()
+        })
+    }
+
+    pub fn set_partial_charges(&mut self, charges: Quantity) {
+        Python::with_gil(|py| {
+            self.0.setattr(py, "partial_charges", charges.0).unwrap();
         })
     }
 }
--- openff-toolkit/src/topology/topology.rs
+++ openff-toolkit/src/topology/topology.rs
@@ -3,7 +3,7 @@ use pyo3::{
     FromPyObject, Py, PyAny, PyObject, Python,
 };
 
-use crate::Molecule;
+use crate::{get_props, Molecule};
 
 const PYMODULE: &str = "openff.toolkit.topology.topology";
 
@@ -21,6 +21,17 @@ impl Topology {
         })
     }
 
+    pub fn from_molecules(molecules: Vec<Molecule>) -> Self {
+        Python::with_gil(|py| {
+            let m = PyModule::import(py, PYMODULE).unwrap();
+            let top = m.getattr("Topology").unwrap();
+            top.call_method1("from_molecules", (molecules,))
+                .unwrap()
+                .extract()
+                .unwrap()
+        })
+    }
+
     pub fn unique_molecules(&self) -> Vec<Molecule> {
         Python::with_gil(|py| {
             let p: PyObject =
@@ -33,5 +44,52 @@ impl Topology {
             }
             ret
         })
+    }
+
+    get_props! {
+        n_unique_molecules, usize;
+        n_molecules, usize;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use openff_units::{unit, Quantity};
+
+    use super::*;
+
+    fn create_ethanol() -> Molecule {
+        let mut ethanol = Molecule::new();
+        ethanol.add_atom(6, 0, false);
+        ethanol.add_atom(6, 0, false);
+        ethanol.add_atom(8, 0, false);
+        ethanol.add_atom(1, 0, false);
+        ethanol.add_atom(1, 0, false);
+        ethanol.add_atom(1, 0, false);
+        ethanol.add_atom(1, 0, false);
+        ethanol.add_atom(1, 0, false);
+        ethanol.add_atom(1, 0, false);
+        ethanol.add_bond(0, 1, 1, false, 1.33);
+        ethanol.add_bond(1, 2, 1, false, 1.23);
+        ethanol.add_bond(0, 3, 1, false, 1.0);
+        ethanol.add_bond(0, 4, 1, false, 1.0);
+        ethanol.add_bond(0, 5, 1, false, 1.0);
+        ethanol.add_bond(1, 6, 1, false, 1.0);
+        ethanol.add_bond(1, 7, 1, false, 1.0);
+        ethanol.add_bond(2, 8, 1, false, 1.0);
+        let charges = Quantity::new(
+            vec![-0.4, -0.3, -0.2, -0.1, 0.00001, 0.1, 0.2, 0.3, 0.4],
+            unit().elementary_charge(),
+        );
+        ethanol.set_partial_charges(charges);
+        ethanol
+    }
+
+    #[test]
+    fn unique_molecules() {
+        let top =
+            Topology::from_molecules(vec![create_ethanol(), create_ethanol()]);
+        assert_eq!(top.n_unique_molecules(), 1);
+        assert_eq!(top.n_molecules(), 2);
     }
 }
